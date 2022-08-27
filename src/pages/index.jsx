113939cf85798294cb684a10400ad65a8f9c16d7
martin0424 <h.newland424@gmail.com>
2022-08-27 12:47:19 -0400

    updated home page and added revalidate for props

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,37 @@
 import { useSession } from "next-auth/react";
+import SneaksApi from "sneaks-api";
 
-function Home() {
+function Home(props) {
   const { data: session, status } = useSession()
 
   if (status === "authenticated") {
     return <p>Signed in as {session.user.email}</p>
   }
+  const products = JSON.parse(props.products)
 
   return (
     <main>
       This is the home page
+      <pre>
+        {JSON.stringify(products, undefined, 2)}
+      </pre>
     </main>
   );
 }
-
+export const getStaticProps = async () => {
+  const sneaks = new SneaksApi()
+  const res = await new Promise((resolve, reject) => {
+    sneaks.getMostPopular(10, (err, products) => {
+      resolve( { 
+        props: {
+          products: JSON.stringify(products)
+        }
+      })
+    })
+  })
+  console.log(res)
+  return res
+}
 // nav bar
 
 //search bar
