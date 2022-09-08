ef498abc7833de191ba3c921b9858fe31ea47a2e
martin0424 <h.newland424@gmail.com>
2022-09-07 20:23:32 -0400

    fixed reload error, added view more button to reload another 10 shoes, worked on mapping to get information most wanted

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,49 +1,79 @@
-import { useSession } from "next-auth/react";
+import { useState } from "react";
 import SneaksApi from "sneaks-api";
 
 function Home(props) {
-  const { data: session, status } = useSession()
-
-  if (status === "authenticated") {
-    return <p>Signed in as {session.user.email}</p>
-  }
+  const [shoeCount, setShoeCount] = useState (10)
+    
+  let finalProducts = [];
   const products = JSON.parse(props.products)
+  
+  
+  for (let i = shoeCount-10; i < shoeCount; i++){
+    finalProducts.push(products[i])
+  }
+  console.log(finalProducts);
 
   return (
     <main>
-      This is the home page
-      <pre>
-        {JSON.stringify(products, undefined, 2)}
-      </pre>
+      Sneaker Studio
+      {/* <pre>
+        {JSON.stringify(finalProducts, undefined, 2)}
+      </pre> */}
+      <div className = "grid ">
+        {
+          finalProducts.map((shoe) => {
+            return (
+              <div key = {shoe._id}>
+                {shoe.shoeName}
+                {shoe.brand}
+                <img src = {shoe.thumbnail} alt = {shoe.shoeName}/> 
+                {shoe.retailPrice}
+                {shoe.releaseDate}
+                {shoe.lowestResellPrice && 
+                <ul>
+                  <li>StockX:{shoe.lowestResellPrice.stockX}</li>
+                  <li>Flight Club:{shoe.lowestResellPrice.flightClub}</li>
+                  <li>GOAT:{shoe.lowestResellPrice.goat}</li>
+                </ul>}
+              </div>
+              
+            )
+          })
+        }
+      </div>
+      <button onClick={() => {
+        setShoeCount((prevShoeCount) => {
+          return prevShoeCount + 10
+        })
+      }}>Show More</button>
     </main>
   );
 }
 export const getStaticProps = async () => {
   const sneaks = new SneaksApi()
   const res = await new Promise((resolve, reject) => {
-    sneaks.getMostPopular(10, (err, products) => {
+    sneaks.getMostPopular(200, (err, products) => {
       resolve( { 
         props: {
           products: JSON.stringify(products)
-        }
+        },
+        revalidate: 600
       })
     })
   })
-  console.log(res)
+  
   return res
 }
-// nav bar
 
-//search bar
+// nav bar?
 
-//nike page
+//search bar- type in brand or style of shoe a list generates of that shoe type (0-10) at a time, usually most popular
 
-//adidas page
-
-//jordan page
-
-//news
+//news (use iFrame)
+// https://sneakernews.com/
 
 //links
+// https://www.stockx.com, https://www.flightclub.com/, https://www.goat.com/. Use company logo for the link instead of just link. (click logo to transfer pages)
+// click on link emblem or name to take you to the cite instead of just the link 
 
 export default Home;
